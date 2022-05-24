fd52aea61b7b541827562ef67da9c2a02f0caf08
Imad Ottallah <imadotta@gmail.com>
2022-05-24 18:37:49 -0500

    fixed mistakes

--- src/api/authorData.js
+++ src/api/authorData.js
@@ -1,10 +1,14 @@
-// import axios from 'axios';
-// import firebaseConfig from './apiKeys';
+import axios from 'axios';
+import firebaseConfig from './apiKeys';
 
-// const dbUrl = firebaseConfig.databaseURL;
+const dbUrl = firebaseConfig.databaseURL;
 
 // FIXME:  GET ALL AUTHORS
-const getAuthors = () => {};
+const getAuthors = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/authors.json`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
 
 // FIXME: CREATE AUTHOR
 const createAuthor = () => {};
